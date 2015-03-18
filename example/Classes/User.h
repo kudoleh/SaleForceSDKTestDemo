3d317c64091cc12a852cb42dffdeccb9579d3067
Oleh Kudinov <oleh@puck.eu>
2015-03-18 17:08:24 +0100

    Created demo project

---
+++ example/Classes/User.h
@@ -0,0 +1,14 @@
+//
+//  User.h
+//  example
+//
+//  Created by Oleh Kudinov on 18/03/15.
+//  Copyright (c) 2015 Puck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SalesforceSDKCore/SFUserAccountManager.h>
+
+@interface User : NSObject <SFUserAccountManagerDelegate>
+
+@end
---
+++ example/Classes/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  example
+//
+//  Created by Oleh Kudinov on 18/03/15.
+//  Copyright (c) 2015 Puck. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
